acdc50cd9f8f528de8d9f12d8f7378234d7ca590
gulaschgoenner <nico.wrage@outlook.de>
2024-06-12 11:09:20 +0200

    Homepage 1

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,18 +3,28 @@ import {QuizShort} from "../utils/types.tsx";
 import {QUIZZES} from "../test/mock/responses.ts";
 import {Helmet} from "react-helmet";
 import Navigation from "../components/Navigation.tsx";
+import {useNavigate} from "react-router-dom";
 
 function HomePage() {
     // const {data, error, isLoading} = useSWR('/quiz', fetcher);
     const data: QuizShort[] = QUIZZES;
+    const navigate = useNavigate();
 
     return (
         <>
             <Helmet title={"Home | BrainBuster"}/>
             <Navigation/>
-            <h2>
-                Home
-            </h2>
+            {data.map(quiz =>
+                <section key={quiz.id} className="quiz-section" onClick={() => {
+                    navigate("/quiz/" + quiz.id)
+                }}>
+                    <h2>{quiz.name}</h2>
+                    <div className="content">
+                        <p>{quiz.description}</p>
+                        <p>Anzahl der Fragen: {quiz.questionCount}</p>
+                    </div>
+                </section>
+            )}
         </>
     )
 }
